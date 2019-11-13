f02f02fd1503aa5f7224f06d19c915b9d46db248
Ramiro Quispe Rampas <ramiroquisper@gmail.com>
2019-11-13 06:53:31 -0500

    LoginViewModel

---
+++ Lands34/Lands/Lands/Infrastructure/Instancelocator.cs
@@ -0,0 +1,21 @@
+﻿namespace Lands.Infrastructure
+{
+    using Lands.ViewModels;
+    class Instancelocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public Instancelocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
